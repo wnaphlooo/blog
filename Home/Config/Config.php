84d22b4f8be334a4bf260c097f654cf0f6135b33
wang <wang@admindeMacBook-Pro.local>
2020-01-04 15:06:37 +0800

    first commit

---
+++ Home/Config/Config.php
@@ -0,0 +1,17 @@
+<?PHP
+
+return array(
+    //初始化数据库
+    'db_type'  =>'mysql',
+    'db_host'  => 'localhost',
+    'db_user'  => 'root',
+    'db_pass' =>'wang',
+    'db_prot' => '3306',
+    'db_name' => 'blog',
+
+    //初始化
+    'defualt_platform' =>'Home',
+    'default_controller' =>'Index',
+    'default_action' => 'index'
+
+);
