b9acf190fa764d1b30ffb633641dd607be54916a
wang <wang@admindeMacBook-Pro.local>
2020-01-04 17:12:03 +0800

    初始化框架

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 define("DS",DIRECTORY_SEPARATOR);
 define("ROOT_PATH",getcwd().DS);
 define("APP_PATH",ROOT_PATH."Home".DS);
-require_once(ROOT_PATH."Frame".DS."Frame.php");
+require_once(ROOT_PATH."Frame".DS."Frame.class.php");
 
 \Frame\Frame::run();
