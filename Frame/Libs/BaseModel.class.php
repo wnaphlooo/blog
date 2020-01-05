0ea6eb114f737952ba4d7ecf96ccdd89aa906b61
wang <wang@admindeMacBook-Pro.local>
2020-01-05 17:30:46 +0800

    pdo框架类

---
+++ Frame/Libs/BaseModel.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Frame\Libs;
+
+use \Frame\Vendor\PDOWrapper;
+
+abstract class BaseModel {
+    protected $pdo = null;
+
+    public function __construct()
+    {
+        $this->pdo = new PDOWrapper();
+    }
+}
